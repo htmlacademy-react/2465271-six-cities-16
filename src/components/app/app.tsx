9757a04a5008f9442bee4946bf44521ce6cc765c
Eyeballeat <eyeballeat@gmail.com>
2024-07-14 22:02:02 +0200

    добавлены элементы favorite

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,20 @@
-import MainPage from '../../pages/main-page/main-page';
-import { CITIES, SortList } from '../../const';
+// import MainPage from '../../pages/main-page/main-page';
+// import { CITIES, SortList } from '../../const';
+import FavoritePage from '../../pages/favorites-page/favorites-page';
 
-type AppProps = {
-  city: typeof CITIES;
-  sortType: typeof SortList;
-}
+// type AppProps = {
+//   city: typeof CITIES;
+//   sortType: typeof SortList;
+// }
 
-function App ({city, sortType}: AppProps): JSX.Element {
+function App (): JSX.Element {
 
   return (
-    <MainPage
-      city = {city}
-      sortType={sortType}
-    />
+    // <MainPage
+    //   city = {city}
+    //   sortType={sortType}
+    // />
+    <FavoritePage/>
   );
 }
 
