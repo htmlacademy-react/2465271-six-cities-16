0416be1ee39ddeb5de435671607342dbaf492c67
Eyeballeat <eyeballeat@gmail.com>
2024-07-15 18:37:36 +0200

    исправил счетчик мест в заголовке главной страницы

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,20 +1,19 @@
-// import MainPage from '../../pages/main-page/main-page';
-// import { CITIES, SortList } from '../../const';
-import FavoritePage from '../../pages/favorites-page/favorites-page';
+import MainPage from '../../pages/main-page/main-page';
+import { CITIES, SortList } from '../../const';
 
-// type AppProps = {
-//   city: typeof CITIES;
-//   sortType: typeof SortList;
-// }
 
-function App (): JSX.Element {
+type AppProps = {
+  city: typeof CITIES;
+  sortType: typeof SortList;
+}
+
+function App ({city, sortType}: AppProps): JSX.Element {
 
   return (
-    // <MainPage
-    //   city = {city}
-    //   sortType={sortType}
-    // />
-    <FavoritePage/>
+    <MainPage
+      city = {city}
+      sortType={sortType}
+    />
   );
 }
 
--- src/components/places-found/places-found.tsx
+++ src/components/places-found/places-found.tsx
@@ -1,6 +1,8 @@
+import { placesOffers } from '../../mocks/places-offers';
+
 function PlacesFound ():JSX.Element {
   return (
-    <b className="places__found">312 places to stay in Amsterdam</b>
+    <b className="places__found">{placesOffers.length} places to stay in Amsterdam</b>
   );
 }
 
