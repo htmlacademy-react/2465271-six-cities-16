a969ca2fe9174b0323dba6b645fbeebcfa974dab
Eyeballeat <eyeballeat@gmail.com>
2024-07-17 11:20:47 +0200

    добавлен NavLink в компонент header-logo

--- src/components/header-logo/header-logo.tsx
+++ src/components/header-logo/header-logo.tsx
@@ -1,9 +1,12 @@
+import { NavLink } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 function HeaderLogo (): JSX.Element {
   return (
     <div className="header__left">
-      <a className="header__logo-link header__logo-link--active">
+      <NavLink to={AppRoute.Main} className="header__logo-link header__logo-link--active">
         <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-      </a>
+      </NavLink>
     </div>
   );
 }
